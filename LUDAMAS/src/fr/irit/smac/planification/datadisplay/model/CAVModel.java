7e1cceefe0949e59b97fe9974359e3b84a6f70c7
MarcillaudG <guilhem.marcillaud@gmail.com>
2020-07-28 09:55:41 +0200

    Test commit

--- LUDAMAS/src/fr/irit/smac/planification/datadisplay/model/CAVModel.java
+++ LUDAMAS/src/fr/irit/smac/planification/datadisplay/model/CAVModel.java
@@ -3,7 +3,7 @@ package fr.irit.smac.planification.datadisplay.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import fr.irit.smac.planification.datadisplay.ui.Modifiable;
+import fr.irit.smac.planification.datadisplay.interfaces.Modifiable;
 import fr.irit.smac.planification.system.CAV;
 
 public class CAVModel {
@@ -30,24 +30,36 @@ public class CAVModel {
 	}
 	
 	public void runExperiment() {
-		
-		Thread taskThread = new Thread(new Runnable() {
-			
-			 public void run() {
-	                cav.generateNewValues(cycle);
-	                while(cycle<1000) {
-	                	if(!pause) {
-	                        oneCycle();
-	                	}
-	                	try {
-	                        Thread.sleep(stepPeriod);
-	                    } catch (InterruptedException e) {
-	                        e.printStackTrace();
-	                    }
-	                }
-	            }
-	        });
-	        taskThread.start();
+
+        Thread taskThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                cav.generateNewValues(cycle);
+                while(cycle<1000) {
+                	if(!pause) {
+                        oneCycle();
+                	}
+                	try {
+                        Thread.sleep(stepPeriod);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        taskThread.start();
+    }
+	
+	/* Pour executer un cycle:
+	 * Si not paused -> mettre sur pause et attendre la fin d'un cycle
+	 * Si paused -> executer le cycle
+	 */
+	public void oneCycle() {
+        cav.manageSituation(cycle);
+        cycle++;
+        updateFrames();
+        cav.generateNewValues(cycle);
 	}
 	
 	public void addModifiables(Modifiable modifiable) {
@@ -55,6 +67,7 @@ public class CAVModel {
 	}
 	
 	public void removeModifiables(Modifiable modifiable) {
+		System.out.println("Modifiable " + modifiable + " removed");
 		modifiables.remove(modifiable);
 	}
 	
